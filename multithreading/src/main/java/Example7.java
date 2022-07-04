437550edf693803da2ce67616a3254bdb64381c8
dzmitrybeinia <dmitribeinia@gmail.com>
2022-07-04 21:45:02 +0300

    example 7

--- multithreading/src/main/java/Example7.java
+++ multithreading/src/main/java/Example7.java
@@ -1,9 +1,13 @@
 public class Example7 {
     public static void main(String[] args) throws InterruptedException {
         System.out.println("main begins");
-        Thread worker = new Thread(new Worker());
-        worker.start();
-        worker.join();
+        Thread worker1 = new Thread(new Worker());
+        worker1.start();
+        worker1.join();
+
+        Thread worker2 = new Thread(new Worker());
+        worker2.start();
+        worker2.join(300);
         System.out.println("main ends");
     }
 }
@@ -11,12 +15,12 @@ public class Example7 {
 class Worker implements Runnable {
     @Override
     public void run() {
-        System.out.println("worker starts");
+        System.out.println(Thread.currentThread().getName() + " starts");
         try {
             Thread.sleep(1000);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
-        System.out.println("worker ends");
+        System.out.println(Thread.currentThread().getName() + " ends");
     }
 }
