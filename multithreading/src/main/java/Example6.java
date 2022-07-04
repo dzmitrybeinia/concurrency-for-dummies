6fef4d14b7dba539b78bdd1caeba450e96a1522f
dzmitrybeinia <dmitribeinia@gmail.com>
2022-07-04 21:39:21 +0300

    example 6

---
+++ multithreading/src/main/java/Example6.java
@@ -0,0 +1,37 @@
+public class Example6 extends Thread {
+    public static void main(String[] args) {
+        Example6 myThread = new Example6();
+        Thread thread = new Thread(new MyRunnable1());
+
+        myThread.start();
+        thread.start();
+
+        System.out.println("Finish");
+    }
+
+    @Override
+    public void run() {
+        for (int i = 0; i < 10; i++) {
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.println(Thread.currentThread().getName() + " " + i);
+        }
+    }
+}
+
+class MyRunnable1 implements Runnable {
+    @Override
+    public void run() {
+        for (int i = 0; i < 10; i++) {
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.println(Thread.currentThread().getName() + " " + i);
+        }
+    }
+}
