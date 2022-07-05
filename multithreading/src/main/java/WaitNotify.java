5dfafcc2bf3ceb66f94693876b93005a6feb0eb3
dzmitrybeinia <dmitribeinia@gmail.com>
2022-07-05 20:32:39 +0300

    wait notify

---
+++ multithreading/src/main/java/WaitNotify.java
@@ -0,0 +1,76 @@
+public class WaitNotify {
+}
+
+class Market {
+    private int breadCount = 0;
+
+    public static void main(String[] args) {
+        Market market = new Market();
+        Producer producer = new Producer(market);
+        Consumer consumer = new Consumer(market);
+
+        Thread producerThread = new Thread(producer);
+        Thread consumerThread = new Thread(consumer);
+
+        producerThread.start();
+        consumerThread.start();
+    }
+
+    public synchronized void getBread() {
+        while (breadCount < 1) {
+            try {
+                wait();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        breadCount--;
+        System.out.println("Consumer get 1 bread");
+        System.out.println("Amount of bread = " + breadCount);
+        notify();
+    }
+
+    public synchronized void putBread() {
+        while (breadCount >= 5) {
+            try {
+                wait();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        breadCount++;
+        System.out.println("Producer add 1 bread");
+        System.out.println("Amount of bread = " + breadCount);
+        notify();
+    }
+}
+
+class Producer implements Runnable {
+    private final Market market;
+
+    public Producer(Market market) {
+        this.market = market;
+    }
+
+    @Override
+    public void run() {
+        for (int i = 0; i < 10; i++) {
+            market.putBread();
+        }
+    }
+}
+
+class Consumer implements Runnable {
+    private final Market market;
+
+    public Consumer(Market market) {
+        this.market = market;
+    }
+
+    @Override
+    public void run() {
+        for (int i = 0; i < 10; i++) {
+            market.getBread();
+        }
+    }
+}
