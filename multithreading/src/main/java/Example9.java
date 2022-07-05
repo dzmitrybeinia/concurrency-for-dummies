2d26e805f21ab75ac703633b88e34e2771aa9f5e
dzmitrybeinia <dmitribeinia@gmail.com>
2022-07-05 18:29:52 +0300

    example 9

---
+++ multithreading/src/main/java/Example9.java
@@ -0,0 +1,26 @@
+public class Example9 {
+    public static void main(String[] args) {
+        MyRunnableEx9 myRunnableEx9 = new MyRunnableEx9();
+        Thread thread1 = new Thread(myRunnableEx9);
+        Thread thread2 = new Thread(myRunnableEx9);
+        Thread thread3 = new Thread(myRunnableEx9);
+
+        thread1.start();
+        thread2.start();
+        thread3.start();
+    }
+}
+
+class Counter {
+    public static int count = 0;
+}
+
+class MyRunnableEx9 implements Runnable {
+    @Override
+    public void run() {
+        for (int i = 0; i < 3; i++) {
+            Counter.count++;
+            System.out.println("counter = " + Counter.count);
+        }
+    }
+}
